1f044ef65a0ab70237657c9e4518abfc969fce22
Ashfak Md. Shibli <sblief.sb@gmail.com>
2016-06-08 10:04:59 +0600

    Added Header Footer Design

--- views/SEIP133704/BookTitle/create.php
+++ views/SEIP133704/BookTitle/create.php
@@ -1,30 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../../../resource/bootstrap-3.3.6/css/bootstrap.min.css">
-    <script src="../../../resource/bootstrap-3.3.6/js/bootstrap.min.js"></script>
-</head>
+<?php include "header.php" ?>
 <title>
     Book
 </title>
+<div class="container">
+    <div class="container-fluid" style="margin-bottom: 250px; margin-top: 100px">
 
-<form class="form-horizontal" role="form" method="post" style="margin-top: 100px" action="store.php">
-    <h2 style="padding-left: 80px; margin-bottom: 20px">
-        Enter Your Book Name
-    </h2>
-    <div class="form-group">
-        <label class="control-label col-sm-2">Enter Book Name</label>
-        <div class="col-sm-6">
-            <input type="text" class="form-control" id="title" name="title" placeholder="Enter Book Name">
-        </div>
+        <form class="form-horizontal" role="form" method="post" style="margin-top: 100px" action="store.php">
+            <h2 style="padding-left: 80px; margin-bottom: 20px">
+                Enter Your Book Name
+            </h2>
+            <div class="form-group">
+                <label class="control-label col-sm-2">Enter Book Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Enter Book Name">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </div>
+            </div>
+
+        </form>
     </div>
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-default">Submit</button>
-        </div>
-    </div>
-</form>
-</html>
+</div>
+<?php include "footer.php"?>
 
---
+++ views/SEIP133704/BookTitle/footer.php
@@ -0,0 +1,11 @@
+<style>
+    /* Set black background color, white text and some padding */
+    footer {
+        background-color: #39554d;
+        color: #c0ffff;
+        padding: 15px;
+    }
+</style>
+<footer class="container-fluid  text-center">
+    <p><span class="glyphicon glyphicon-copyright-mark"></span> Copyright Atomic Projects 2016</p>
+</footer>
