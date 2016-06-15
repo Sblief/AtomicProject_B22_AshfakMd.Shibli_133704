c31b1df54b655baedba6026599affe1f3d98132f
Ashfak Md. Shibli <sblief.sb@gmail.com>
2016-06-15 19:33:20 +0600

    Main Index Added

--- index.php
+++ index.php
@@ -1,7 +1,121 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Ashfak Md. Shibli
- * Date: 06-06-16
- * Time: PM 10.28
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="resource/bootstrap-3.3.6/css/bootstrap.min.css">
+    <script src="resource/bootstrap-3.3.6/js/bootstrap.min.js"></script>
+    <script src="resource/jquery/jquery-3.0.0.min.js"></script>
+    <title>Atomic Project</title>
+<div class="container">
+
+<div class="container-fluid" >
+    <div class="panel-default">
+        <div class="panel-heading">
+            <h2>Atomic Projects</h2>
+            <h3>Name: Ashfak Md. Shibli</h3>
+            <h3>SEID: 133704</h3>
+            <h3>PHP-B22</h3>
+        </div>
+        <div class="panel-body">
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <th>
+                            SL
+                        </th>
+                        <th>
+                            Project Name
+                        </th>
+                        <th>
+                            View
+                        </th>
+                    </tr>
+                <tr>
+                    <td>1</td>
+                    <td>Book (Text)</td>
+                    <td>
+                        <a href="views/SEIP133704/BookTitle" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                    </td>
+                </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Birthday (Date)</td>
+                        <td>
+                            <a href="views/SEIP133704/DateOfBirth" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Summary of organization (Textarea)</td>
+                        <td>
+                            <a href="views/SEIP133704/OrganizationSummary" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>Newsletter (Email)</td>
+                        <td>
+                            <a href="views/SEIP133704/NewsLetter" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>Profile Pic (File)</td>
+                        <td>
+                            <a href="views/SEIP133704/Profile" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>Education Level (Radio) </td>
+                        <td>
+                            <a href="views/SEIP133704/EducationLevel" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>Terms and Condition (Checkbox-Single)</td>
+                        <td>
+                            <a href="views/SEIP133704/TermsAndCondition" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>Hobby (CheckBoxMultiple)</td>
+                        <td>
+                            <a href="views/SEIP133704/Hobby" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>9</td>
+                        <td>City (Select)</td>
+                        <td>
+                            <a href="views/SEIP133704/City" target="_blank"><button class="btn btn-success">Click to View</button></a>
+                        </td>
+                    </tr>
+
+
+                </tbody>
+
+            </table>
+
+        </div>
+
+
+
+    </div>
+
+    </div>
+    </div>
+    <style>
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #39554d;
+            color: #c0ffff;
+            padding: 15px;
+        }
+    </style>
+    <footer class="container-fluid  text-center">
+        <p><span class="glyphicon glyphicon-copyright-mark"></span> Copyright Atomic Projects 2016</p>
+    </footer>
