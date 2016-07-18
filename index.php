11515b01e8d101af78cf290cc3d46007f8de500c
Ashfak Md. Shibli <sblief.sb@gmail.com>
2016-07-18 22:43:40 +0600

    All Done Third Party, Search, Filter

--- index.php
+++ index.php
@@ -144,7 +144,7 @@
 <nav role="navigation" class="navbar navbar-default navbar-fixed-bottom">
     <footer class="container-fluid  text-center">
         <div class="container">
-            <p><span class="glyphicon glyphicon-copyright-mark"></span> Copyright Ashfak Md. Shibli & Atomic Projects 2016</p>
+            <p><span class="fa fa-copyright"></span> Copyright Ashfak Md. Shibli & Atomic Projects 2016</p>
         </div>
     </footer>
 </nav>
